e971ec2572b12b88af85f224d023fb70043b6342
roy769 <roy@mortgallery.com>
2017-09-07 15:53:03 -0700

    werk it

---
+++ BasicCard.js
@@ -0,0 +1,19 @@
+  var Front = require("./front")
+
+
+
+      var Basic = function frontToggle(back, front)
+      {
+          //allow global string to be inputted
+        var questions = [];
+
+          this.questionOne = questionOne;
+          this.questionOne = questionTwo; this.questionOne = questionThree;
+          this.questionOne = questionFour ;
+
+            this.addQuestion = function ()
+              {
+
+              }
+
+      }
---
+++ ClozeCard.js
@@ -0,0 +1,21 @@
+var Back = require("./back")
+
+
+
+      function backToggle()
+      {
+
+      var answer = [];
+
+        this.backOne = "answerOne" ;
+        this.backTwo = "answerTwo"; ,this.backThree = "answerThree" ,this.backFour = "answerFour" ;
+
+          this.addAnswer = function ()
+            {
+
+            }
+      }
+
+//if question side is chosen put answer on back
+
+//on click load next
